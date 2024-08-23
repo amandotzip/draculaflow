c0f1ee71dc860bdce914b151062757c3a548b9bb
Amanuel Awoke <amanuelonline@gmail.com>
2024-08-22 21:40:48 -0400

    fixed formatting added background

--- draculaflow/src/App.js
+++ draculaflow/src/App.js
@@ -4,12 +4,23 @@ import AudioVisualizer from './components/AudioVisualizer';
 import BackgroundImageComponent from './components/BackgroundImageComponent';
 
 function App() {
+  const backgroundImage = `${process.env.PUBLIC_URL}/background.jpg`;
+  console.log('Background Image Path:', backgroundImage); 
   return (
-    <div className="App">
-      {/* <BackgroundImageComponent /> */}
+    <div style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      width: '100%',
+      height: '100vh'
+    }}
+      // className="App"
+      >
+
       <header className="App-header">
-      <h1>DRACULA FLOW SCRIPTURE</h1>
-        <img src="/portrait.jpg" alt="My Description" style={{ width: '50%', maxWidth: '500px', height: 'auto' }} />
+        <h1>DRACULA FLOW SCRIPTURE</h1>
+        <img src={`${process.env.PUBLIC_URL}/portrait.jpg`} alt="My Description" style={{ width: '50%', maxWidth: '500px', height: 'auto' }} />
         <p>
         </p>
         <a
@@ -21,7 +32,7 @@ function App() {
         </a>
         <AudioVisualizer /> {/* Use the AudioButton component */}
       </header>
-      
+
     </div>
 
 
